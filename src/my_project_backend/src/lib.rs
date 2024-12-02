b00e9bea83a8f210ffd8830e624e8e8fa4a0f529
Piotr Jakóbczyk <piotrekjakobczyk1@gmail.com>
2024-12-02 17:51:36 +0000

    Created message storage

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,22 @@
+use std::{borrow::BorrowMut, cell::RefCell};
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
+#[ic_cdk_macros::query]
+fn get_msg() -> String{
+    MSG.with(|msg| (*msg.borrow()).clone())
+}
+
+#[ic_cdk_macros::update]
+fn set_msg(new_msg : String) -> () {
+    MSG.with(|msg| *msg.borrow_mut() = new_msg)
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+ic_cdk::export_candid!();
